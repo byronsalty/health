f6c12d5f6df69663dfe5016c503d70079e8a3bcb
Byron Salty <byronsalty@gmail.com>
2022-11-25 19:47:00 -0600

    updated form logic

--- lib/health_web/controllers/form_controller.ex
+++ lib/health_web/controllers/form_controller.ex
@@ -5,6 +5,16 @@ defmodule HealthWeb.FormController do
     render(conn, "index.html")
   end
   def submit(conn, _params) do
-    render(conn, "index.html")
+    pass = (conn.params["breastfed"] == "Y")
+      && (conn.params["fullterm"] == "Y")
+      && (conn.params["postdischarge"] == "Y")
+      && (conn.params["recent"] == "Y")
+      && (conn.params["english"] == "Y")
+      && (conn.params["country"] == "Y")
+    if pass do
+      render(conn, "accepted.html")
+    else
+      render(conn, "rejected.html")
+    end
   end
 end
