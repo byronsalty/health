76e662271c1f31a57beecf677d81d12293d066c1
Byron Salty <byronsalty@gmail.com>
2022-11-26 11:26:25 -0600

    some deployment tweaks

---
+++ addEnv.sh
@@ -0,0 +1,8 @@
+export POOL_SIZE=2
+export PORT=4001
+#export DATABASE_URL=ecto://postgres:postgres@localhost/saturn_dev
+export SECRET_KEY_BASE=$(mix phx.gen.secret)
+
+export PHX_HOST=localhost
+export PHX_SERVER=true
+
